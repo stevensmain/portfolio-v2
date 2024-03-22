9071a0d0f8d022b105cb3ee1f6f21881d00bbfe6
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-03-22 18:33:54 -0400

    projects changed

--- src/mocks/projects.js
+++ src/mocks/projects.js
@@ -3,22 +3,22 @@ const projects = [
     title: "Kairos - Landing page",
     description:
       "Landing page para compañia inmobiliaria ubicada en Medellin, desarrollado en Next.js y TailwindCSS. Esta integra conexion con un servicio para el manejo de correos utilizando EmailJS.",
-    url: "https://kairos.aequus.com.co/",
+    url: "https://kairos.aequus.com.co",
     img: "/img/kairos.png",
   },
   {
     title: "Spotify - Astro clone",
     description:
       "Clon de la aplicacion web de Spotify desarrollado en Astro utilizando la integracion con React.js, ademas de funcionalidades como View Transitions y un estado global usando Zustand.",
-    url: "https://astro-spotify-clone.netlify.app/",
+    url: "https://astro-spotify-clone.netlify.app",
     img: "/img/astro-spotify.png",
   },
   {
-    title: "8I - Landing page",
+    title: "Twiiter Clone",
     description:
-      "Landing page para compañia orientada al desarrollo de soluciones tecnologicas basada en Chile, desarrollada en React.js. Esta integra conexion con un servicio para el manejo de correos utilizando EmailJS.",
-    url: "https://www.8i.cl/",
-    img: "/img/8i.png",
+      "Clon parcial de la plataforma X, desarrollado utilizando Next.js en su version 14, junto con tailwindcss y supabase.",
+    url: "https://stv-x-clone.vercel.app",
+    img: "/img/x-clone.png",
   },
 ]
 
