95bd4d35389e3a528db96436c92f03193dded740
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-03-22 19:02:31 -0400

    projects changes

--- src/mocks/projects.js
+++ src/mocks/projects.js
@@ -2,21 +2,28 @@ const projects = [
   {
     title: "Kairos - Landing page",
     description:
-      "Landing page para compañia inmobiliaria ubicada en Medellin, desarrollado en Next.js y TailwindCSS. Esta integra conexion con un servicio para el manejo de correos utilizando EmailJS.",
+      "Landing page para una compañía inmobiliaria ubicada en Medellín. Desarrollada con Next.js y Tailwind CSS. Además, integra la funcionalidad de conexión con un servicio de manejo de correos del utilizando EmailJS.",
     url: "https://kairos.aequus.com.co",
     img: "/img/kairos.png",
   },
   {
     title: "Spotify - Astro clone",
     description:
-      "Clon de la aplicacion web de Spotify desarrollado en Astro utilizando la integracion con React.js, ademas de funcionalidades como View Transitions y un estado global usando Zustand.",
+      "Desarrollado en Astro y aprovechando la integración con React.js. He incorporado funcionalidades avanzadas como las transiciones de vista (View Transitions) para una experiencia de usuario más fluida y atractiva. Además, he implementado un estado global utilizando Zustand, garantizando una gestión eficiente y coherente de los datos en toda la aplicación.",
     url: "https://astro-spotify-clone.netlify.app",
     img: "/img/astro-spotify.png",
   },
   {
-    title: "Twiiter Clone",
+    title: "TopAnime App",
     description:
-      "Clon parcial de la plataforma X, desarrollado utilizando Next.js en su version 14, junto con tailwindcss y supabase.",
+      "Landing page que presenta un catálogo dinámico con los animes más populares y emocionantes de la actualidad. Utilizando tecnologías de vanguardia como Next.js, Tailwind CSS y Framer Motion para crear una experiencia de usuario fluida y atractiva.",
+    url: "https://topanime-stv.netlify.app",
+    img: "/img/topanime.png",
+  },
+  {
+    title: "Partial Twitter Clone",
+    description:
+      "He desarrollado esta versión utilizando Next.js, Tailwind CSS y Supabase. Con Next.js, garantizo un rendimiento óptimo y una navegación fluida. Tailwind CSS me ha permitido diseñar una interfaz elegante y moderna que se adapta perfectamente a cualquier dispositivo. Además, he integrado Supabase para gestionar la base de datos y la autenticación, asegurando así la seguridad y la eficiencia de la plataforma.",
     url: "https://stv-x-clone.vercel.app",
     img: "/img/x-clone.png",
   },
