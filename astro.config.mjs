889b76906c3f848b619badb8b549c303d24bf414
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2025-06-14 01:25:32 -0400

    chore: update project configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import netlify from '@astrojs/netlify/functions'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   output: 'server',
   adapter: netlify(),
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
